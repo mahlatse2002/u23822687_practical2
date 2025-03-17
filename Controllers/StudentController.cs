9e60240645545523cc9d70c7fe99bd5a66a9a998
Mahlatse Ndzumeni <u23822687@up.ac.za>
2025-03-17 19:56:04 +0200

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace Practical2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult GroupList()
+        {
+
+            List<Models.StudentModel> ClassList = new List<Models.StudentModel>();
+
+
+            ClassList.Add(new Models.StudentModel { FirstName = "Mahlatse", LastName = "Ndzumeni", StudentNumber = "23822687", Email = "U23822687@Tuks.co.za" });
+            ClassList.Add(new Models.StudentModel { FirstName = "Terry", LastName = "Ntimane", StudentNumber = "24827643", Email = "u24827643@Tuks.co.za" });
+            ClassList.Add(new Models.StudentModel { FirstName = "Ethan", LastName = "Carlson", StudentNumber = "23628654", Email = "u23628654@Tuks.co.za" });
+            ClassList.Add(new Models.StudentModel { FirstName = "priyanka", LastName = "maharaj", StudentNumber = "25972883", Email = "u25972883@Tuks.co.za" });
+            ClassList.Add(new Models.StudentModel { FirstName = "Lebogang", LastName = "Mokoena", StudentNumber = "22829627", Email = "u22829627@Tuks.co.za" });
+
+            return View(ClassList);
+
+
+
+
+
+
+        }
+    }
+}
+
---
+++ Models/StudentModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practical2.Models
+{
+    public class StudentModel
+    {
+
+
+
+        [Display(Name = "first NAME")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "last NAME")]
+        public string LastName { get; set; }
+
+
+        [Display(Name = "student number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "email")]
+        public string Email { get; set; }
+
+        
+        
+        
+
+    }
+}
+
+
